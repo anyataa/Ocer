59f4d52aaa687434bb4f04decfd48575cda8593b
Anya Tamara <anyatmr@gmail.com>
2021-06-03 07:48:13 +0700

    Initial Commit

---
+++ Ocer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ocer
+//
+//  Created by Anya Akbar on 03/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
