4a78184ee9b1e2303e1a3cd34d44a6c589926684
Stefan Adisurya <stefanadisurya@gmail.com>
2021-06-15 19:57:57 +0700

    fix CardView and modify Menyikat Gigi

--- Ocer/MandiViewController.swift
+++ Ocer/MandiViewController.swift
@@ -6,63 +6,87 @@
 //
 
 import UIKit
-import AVKit
-import AVFoundation
 
 class MandiViewController: UIViewController {
     
     var center: CGPoint = CGPoint.zero
     var zones: [CGRect] = []
     
-    var player: AVPlayer?
-    
-//    let screenSize = UIScreen.main.boundss
-//    let screenWidth = screenSize.width
-//    let screenHeight = screenSize.height
-    
-    let videoView1: UIView = {
-        let view = UIView(frame: CGRect(x: (UIScreen.main.bounds.width - UIScreen.main.bounds.width + 130), y: (UIScreen.main.bounds.height - UIScreen.main.bounds.height + 736), width: 300, height: 150))
-        view.backgroundColor = .green
+    let scene1: UIView = {
+        let view = UIView(frame: CGRect(x: 55, y: 700, width: 300, height: 200))
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "Mandi2")!)
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 8
         view.layer.cornerRadius = 20
         
         return view
     }()
     
-    let videoView2: UIView = {
-        let view = UIView(frame: CGRect(x: (UIScreen.main.bounds.width - UIScreen.main.bounds.width + 524), y: 736, width: (UIScreen.main.bounds.width - UIScreen.main.bounds.width + 300), height: 150))
-        view.backgroundColor = .orange
+    let scene2: UIView = {
+        let view = UIView(frame: CGRect(x: 375, y: 700, width: 300, height: 200))
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "Mandi4")!)
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 8
         view.layer.cornerRadius = 20
         
         return view
     }()
     
-    let videoView3: UIView = {
-        let view = UIView(frame: CGRect(x: (UIScreen.main.bounds.width - UIScreen.main.bounds.width + 937), y: (UIScreen.main.bounds.width - UIScreen.main.bounds.width + 736), width: 300, height: 150))
-        view.backgroundColor = .systemIndigo
+    let scene3: UIView = {
+        let view = UIView(frame: CGRect(x: 695, y: 700, width: 300, height: 200))
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "Mandi1")!)
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 8
         view.layer.cornerRadius = 20
         
         return view
     }()
     
-    let videoZone1: UIView = {
-        let view = UIView(frame: CGRect(x: (UIScreen.main.bounds.width - UIScreen.main.bounds.width + 130), y: (UIScreen.main.bounds.width - UIScreen.main.bounds.width + 136), width: 300, height: 150))
-        view.backgroundColor = .lightGray
+    let scene4: UIView = {
+        let view = UIView(frame: CGRect(x: 1015, y: 700, width: 300, height: 200))
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "Mandi3")!)
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 8
         view.layer.cornerRadius = 20
         
         return view
     }()
     
-    let videoZone2: UIView = {
-        let view = UIView(frame: CGRect(x: (UIScreen.main.bounds.width - UIScreen.main.bounds.width + 524), y: (UIScreen.main.bounds.width - UIScreen.main.bounds.width + 136), width: 300, height: 150))
-        view.backgroundColor = .lightGray
+    let sceneZone1: UIView = {
+        let view = UIView(frame: CGRect(x: 55, y: 200, width: 300, height: 200))
+        view.backgroundColor = .white
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 8
         view.layer.cornerRadius = 20
         
         return view
     }()
     
-    let videoZone3: UIView = {
-        let view = UIView(frame: CGRect(x: (UIScreen.main.bounds.width - UIScreen.main.bounds.width + 937), y: (UIScreen.main.bounds.width - UIScreen.main.bounds.width + 136), width: 300, height: 150))
-        view.backgroundColor = .lightGray
+    let sceneZone2: UIView = {
+        let view = UIView(frame: CGRect(x: 375, y: 200, width: 300, height: 200))
+        view.backgroundColor = .white
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 8
+        view.layer.cornerRadius = 20
+        
+        return view
+    }()
+    
+    let sceneZone3: UIView = {
+        let view = UIView(frame: CGRect(x: 695, y: 200, width: 300, height: 200))
+        view.backgroundColor = .white
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 8
+        view.layer.cornerRadius = 20
+        
+        return view
+    }()
+    
+    let sceneZone4: UIView = {
+        let view = UIView(frame: CGRect(x: 1015, y: 200, width: 300, height: 200))
+        view.backgroundColor = .white
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 8
         view.layer.cornerRadius = 20
         
         return view
@@ -71,43 +95,44 @@ class MandiViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(videoView1)
-        view.addSubview(videoView2)
-        view.addSubview(videoView3)
+        view.addSubview(scene1)
+        view.addSubview(scene2)
+        view.addSubview(scene3)
+        view.addSubview(scene4)
         
-        view.addSubview(videoZone1)
-        view.addSubview(videoZone2)
-        view.addSubview(videoZone3)
+        view.addSubview(sceneZone1)
+        view.addSubview(sceneZone2)
+        view.addSubview(sceneZone3)
+        view.addSubview(sceneZone4)
         
-        zones.append(videoView1.frame)
-        zones.append(videoView2.frame)
-        zones.append(videoView3.frame)
+        zones.append(sceneZone1.frame)
+        zones.append(sceneZone2.frame)
+        zones.append(sceneZone3.frame)
+        zones.append(sceneZone4.frame)
         
-        videoView1.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragVideo1(_:))))
-        videoView2.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragVideo2(_:))))
-        videoView3.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragVideo3(_:))))
-        
-//        playVideo()
+        scene1.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragScene1(_:))))
+        scene2.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragScene2(_:))))
+        scene3.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragScene3(_:))))
+        scene4.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragScene4(_:))))
     }
     
-    @objc func dragVideo1(_ sender: UIPanGestureRecognizer) {
-        player?.pause()
+    @objc func dragScene1(_ sender: UIPanGestureRecognizer) {
         switch sender.state {
         case .began:
-            center = videoView1.center
-            
+            center = scene1.center
+
         case .changed:
             view.bringSubviewToFront(sender.view!)
-            
+
             let translation = sender.translation(in: view)
-            videoView1.center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
-            
+            scene1.center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
+
             for zone in zones {
                 if (sender.view?.frame != nil) {
                     if (sender.view!.frame.intersects(zone)) {
                         sender.view!.layer.shadowRadius = 15
-                        
-                        if (zone == videoZone3.frame) {
+
+                        if (zone == sceneZone2.frame) {
                             sender.view!.layer.shadowColor = UIColor.green.cgColor
                         } else {
                             sender.view!.layer.shadowColor = UIColor.red.cgColor
@@ -115,102 +140,149 @@ class MandiViewController: UIViewController {
                         sender.view!.layer.shadowOpacity = 1
                         break
                     } else {
-                        sender.view!.layer.shadowOpacity = 1
+                        sender.view!.layer.shadowOpacity = 0
                     }
                 }
             }
-            
+
         case .ended:
             UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseIn]) {
                 if (sender.view?.frame != nil) {
-                    self.dragEnded(draggableObject: sender, draggableZone: self.videoZone3)
+                    self.dragEnded(draggableObject: sender, draggableZone: self.sceneZone2)
                 }
             }
-            
+
+        default:
+            break
+        }
+    }
+
+    @objc func dragScene2(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            center = scene2.center
+
+        case .changed:
+            view.bringSubviewToFront(sender.view!)
+
+            let translation = sender.translation(in: view)
+            scene2.center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
+
+            for zone in zones {
+                if (sender.view?.frame != nil) {
+                    if (sender.view!.frame.intersects(zone)) {
+                        sender.view!.layer.shadowRadius = 15
+
+                        if (zone == sceneZone4.frame) {
+                            sender.view!.layer.shadowColor = UIColor.green.cgColor
+                        } else {
+                            sender.view!.layer.shadowColor = UIColor.red.cgColor
+                        }
+                        sender.view!.layer.shadowOpacity = 1
+                        break
+                    } else {
+                        sender.view!.layer.shadowOpacity = 0
+                    }
+                }
+            }
+
+        case .ended:
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseIn]) {
+                if (sender.view?.frame != nil) {
+                    self.dragEnded(draggableObject: sender, draggableZone: self.sceneZone4)
+                }
+            }
+
+        default:
+            break
+        }
+    }
+
+    @objc func dragScene3(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            center = scene3.center
+
+        case .changed:
+            view.bringSubviewToFront(sender.view!)
+
+            let translation = sender.translation(in: view)
+            scene3.center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
+
+            for zone in zones {
+                if (sender.view?.frame != nil) {
+                    if (sender.view!.frame.intersects(zone)) {
+                        sender.view!.layer.shadowRadius = 15
+
+                        if (zone == sceneZone1.frame) {
+                            sender.view!.layer.shadowColor = UIColor.green.cgColor
+                        } else {
+                            sender.view!.layer.shadowColor = UIColor.red.cgColor
+                        }
+                        sender.view!.layer.shadowOpacity = 1
+                        break
+                    } else {
+                        sender.view!.layer.shadowOpacity = 0
+                    }
+                }
+            }
+
+        case .ended:
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseIn]) {
+                if (sender.view?.frame != nil) {
+                    self.dragEnded(draggableObject: sender, draggableZone: self.sceneZone1)
+                }
+            }
+
+        default:
+            break
+        }
+    }
+
+    @objc func dragScene4(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            center = scene4.center
+
+        case .changed:
+            view.bringSubviewToFront(sender.view!)
+
+            let translation = sender.translation(in: view)
+            scene4.center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
+
+            for zone in zones {
+                if (sender.view?.frame != nil) {
+                    if (sender.view!.frame.intersects(zone)) {
+                        sender.view!.layer.shadowRadius = 15
+
+                        if (zone == sceneZone3.frame) {
+                            sender.view!.layer.shadowColor = UIColor.green.cgColor
+                        } else {
+                            sender.view!.layer.shadowColor = UIColor.red.cgColor
+                        }
+                        sender.view!.layer.shadowOpacity = 1
+                        break
+                    } else {
+                        sender.view!.layer.shadowOpacity = 0
+                    }
+                }
+            }
+
+        case .ended:
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseIn]) {
+                if (sender.view?.frame != nil) {
+                    self.dragEnded(draggableObject: sender, draggableZone: self.sceneZone3)
+                }
+            }
+
         default:
             break
         }
     }
     
-    @objc func dragVideo2(_ sender: UIPanGestureRecognizer) {
-        switch sender.state {
-        case .began:
-            center = videoView2.center
-            
-        case .changed:
-            view.bringSubviewToFront(sender.view!)
-            
-            let translation = sender.translation(in: view)
-            videoView2.center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
-            
-            for zone in zones {
-                if (sender.view?.frame != nil) {
-                    if (sender.view!.frame.intersects(zone)) {
-                        sender.view!.layer.shadowRadius = 15
-                        
-                        if (zone == videoZone1.frame) {
-                            sender.view!.layer.shadowColor = UIColor.green.cgColor
-                        } else {
-                            sender.view!.layer.shadowColor = UIColor.red.cgColor
-                        }
-                        sender.view!.layer.shadowOpacity = 1
-                        break
-                    } else {
-                        sender.view!.layer.shadowOpacity = 1
-                    }
-                }
-            }
-            
-        case .ended:
-            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseIn]) {
-                if (sender.view?.frame != nil) {
-                    self.dragEnded(draggableObject: sender, draggableZone: self.videoZone1)
-                }
-            }
-            
-        default:
-            break
-        }
-    }
-    
-    @objc func dragVideo3(_ sender: UIPanGestureRecognizer) {
-        switch sender.state {
-        case .began:
-            center = videoView3.center
-            
-        case .changed:
-            view.bringSubviewToFront(sender.view!)
-            
-            let translation = sender.translation(in: view)
-            videoView3.center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
-            
-            for zone in zones {
-                if (sender.view?.frame != nil) {
-                    if (sender.view!.frame.intersects(zone)) {
-                        sender.view!.layer.shadowRadius = 15
-                        
-                        if (zone == videoZone2.frame) {
-                            sender.view!.layer.shadowColor = UIColor.green.cgColor
-                        } else {
-                            sender.view!.layer.shadowColor = UIColor.red.cgColor
-                        }
-                        sender.view!.layer.shadowOpacity = 1
-                        break
-                    } else {
-                        sender.view!.layer.shadowOpacity = 1
-                    }
-                }
-            }
-            
-        case .ended:
-            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseIn]) {
-                if (sender.view?.frame != nil) {
-                    self.dragEnded(draggableObject: sender, draggableZone: self.videoZone2)
-                }
-            }
-            
-        default:
-            break
+    func check() {
+        if (self.scene1.center == self.sceneZone2.center) && (self.scene2.center == self.sceneZone4.center) && (self.scene3.center == self.sceneZone1.center) && (self.scene4.center == self.sceneZone3.center) {
+            print("All correct")
         }
     }
     
@@ -218,41 +290,20 @@ class MandiViewController: UIViewController {
         if (draggableObject.view!.frame.intersects(draggableZone.frame)) {
             draggableObject.view!.center = draggableZone.center
         } else {
-            draggableObject.view!.center = CGPoint(x: Int.random(in: 30...900), y: Int.random(in: 50...600))
+            if draggableObject.view == scene1 {
+                draggableObject.view!.center = CGPoint(x: 205, y: 800)
+            } else if draggableObject.view == scene2 {
+                draggableObject.view!.center = CGPoint(x: 525, y: 800)
+            } else if draggableObject.view == scene3 {
+                draggableObject.view!.center = CGPoint(x: 845, y: 800)
+            } else {
+                draggableObject.view!.center = CGPoint(x: 1165, y: 800)
+            }
         }
         
-        player?.play()
+        check()
         
         draggableObject.view!.layer.shadowOpacity = 0
-    }
-    
-    private func playVideo() {
-        guard let path = Bundle.main.path(forResource: "video2", ofType: "mp4") else {
-            debugPrint("Not found")
-            return
-        }
-        
-        player = AVPlayer(url: URL(fileURLWithPath: path))
-
-        let playerLayer = AVPlayerLayer(player: player)
-        playerLayer.frame = videoView1.bounds
-        playerLayer.videoGravity = .resizeAspectFill
-        playerLayer.cornerRadius = 20
-        self.videoView1.layer.addSublayer(playerLayer)
-        
-        self.player?.addObserver(self, forKeyPath: "rate", options: .new, context: nil)
-        
-        player?.play()
-//        player?.pause()
-    }
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        // Bikin button play dan pause di observeValue.
-        if self.player?.rate == 0 {
-            print("Pause") // Button pause
-        } else {
-            print("Play") // Button play
-        }
     }
     
 }
