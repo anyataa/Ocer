e2271b16aaf0cfab2cbb1248a7515606a7b420f2
Anya Tamara <anyatmr@gmail.com>
2021-06-15 19:42:01 +0700

    Delete mobil.png initial commit implements XIB Aufar

--- Ocer/SarapanViewController.swift
+++ Ocer/SarapanViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SarapanViewController: UIViewController {
     var center: CGPoint = CGPoint.zero
+    var score : Int = 0
     var dropZone : CGRect = CGRect(x: 0, y: 0, width: 0, height: 0)
     func setBackground() {
         let background = UIImageView(image: UIImage(named: "backgroundMakan"))
@@ -71,7 +72,7 @@ class SarapanViewController: UIViewController {
     let car: UIView  = {
         let mobil = UIView(frame: CGRect(x: 500, y: 50, width: 289, height: 210))
 //      NOTE:  Remove force Unwrap
-          mobil.backgroundColor = UIColor(patternImage: UIImage(named: "Mobil")!)
+        mobil.backgroundColor = UIColor(patternImage: UIImage(named: "mobil")!)
             return mobil
     }()
     
@@ -99,22 +100,24 @@ class SarapanViewController: UIViewController {
         
         
 //        Gesture
-        ball.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragBall(_:))))
+        ball.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragNotFood(_:))))
+        car.addGestureRecognizer((UIPanGestureRecognizer(target: self, action: #selector(dragNotFood(_:)))))
+        telur.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragFood(_:))))
+        ayam.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragFood(_:))))
+        sayur.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragFood(_:))))
         
-        car.addGestureRecognizer((UIPanGestureRecognizer(target: self, action: #selector(dragCar(_:)))))
+//      Saying Congratulation
         
-        telur.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragTelur(_:))))
-        ayam.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragAyam(_:))))
-        sayur.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragSayur(_:))))
+        
         
     }
     
 //    Tanya Kak Zein Masukin Param di sini GIMANA? Tanya temen temen juga pas async !!!
-    @objc private func dragBall(_ sender: UIPanGestureRecognizer) {
+    @objc private func dragNotFood(_ sender: UIPanGestureRecognizer) {
         switch sender.state {
         case .began:
             //get object position on drag start
-            center = ball.center
+            center = sender.view!.center
             print("began")
         
         case .changed:
@@ -125,7 +128,7 @@ class SarapanViewController: UIViewController {
             //update object position while dragging
             let translation = sender.translation(in: view)
             print("translate")
-            ball.center = CGPoint(x: center.x + translation.x,
+            sender.view!.center = CGPoint(x: center.x + translation.x,
                                           y: center.y + translation.y)
 
         case .ended:
@@ -144,49 +147,18 @@ class SarapanViewController: UIViewController {
         }
     }
     
-    @objc private func dragCar(_ sender: UIPanGestureRecognizer) {
-        switch sender.state {
-        case .began:
-            //get object position on drag start
-            center = car.center
-            print("began")
-        
-        case .changed:
-            //bring dragged object to front (topmost z index within the same view hierarchy)
-            view.bringSubviewToFront(sender.view!)
-            print("change")
-            
-            //update object position while dragging
-            let translation = sender.translation(in: view)
-            print("translate")
-            car.center = CGPoint(x: center.x + translation.x,
-                                          y: center.y + translation.y)
-
-        case .ended:
-          
-            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseIn]) {
-                if (sender.view?.frame != nil) {
-                    sender.view!.center = CGPoint(x: Int.random(in: 30...900), y: Int.random(in: 50...600))
-//            EXECUTE SOUND EFFECT BOING!
-                   
-                
-                }
-            }
-          
-        default:
-            break
-        }
-    }
+  
     
-    @objc private func dragTelur(_ sender: UIPanGestureRecognizer) {
+    @objc private func dragFood(_ sender: UIPanGestureRecognizer) {
         switch sender.state {
         case .began:
-            center = telur.center
+            center = sender.view!.center
+           
         case .changed:
 //            NOTE : Remove Force unwrap
             view.bringSubviewToFront(sender.view!)
             let translation = sender.translation(in: view)
-            telur.center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
+            sender.view!.center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
             if sender.view?.frame != nil {
                 
                 if sender.view!.frame.intersects(dropZone) {
@@ -201,8 +173,10 @@ class SarapanViewController: UIViewController {
             
         case .ended:
             UIView.animate(withDuration: 0.5, delay: 0.0,usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseIn]){
-               
                 self.dragEnded(draggableObject: sender, draggableZone: self.bowlZone)
+                self.score += 1
+                print(self.score)
+                self.sayCongratulationWhenFinish()
             }
             
         default:
@@ -210,77 +184,14 @@ class SarapanViewController: UIViewController {
         }
     }
     
-    @objc private func dragSayur(_ sender: UIPanGestureRecognizer) {
-        switch sender.state {
-        case .began:
-            center = sayur.center
-        case .changed:
-//            NOTE : Remove Force unwrap
-            view.bringSubviewToFront(sender.view!)
-            let translation = sender.translation(in: view)
-            sayur.center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
-            if sender.view?.frame != nil {
-                
-                if sender.view!.frame.intersects(dropZone) {
-                    sender.view!.layer.shadowRadius = 15
-                    sender.view!.layer.shadowColor = UIColor.systemGreen.cgColor
-                } else {
-                    
-                }
-                sender.view!.layer.shadowOpacity = 1
-                break
-            }
-            
-        case .ended:
-            UIView.animate(withDuration: 0.5, delay: 0.0,usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseIn]){
-               
-                self.dragEnded(draggableObject: sender, draggableZone: self.bowlZone)
-            }
-            
-        default:
-            break
-        }
-    }
-    
-    @objc private func dragAyam(_ sender: UIPanGestureRecognizer) {
-        switch sender.state {
-        case .began:
-            center = ayam.center
-        case .changed:
-//            NOTE : Remove Force unwrap
-            view.bringSubviewToFront(sender.view!)
-            let translation = sender.translation(in: view)
-           ayam.center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
-            if sender.view?.frame != nil {
-                
-                if sender.view!.frame.intersects(dropZone) {
-                    sender.view!.layer.shadowRadius = 15
-                    sender.view!.layer.shadowColor = UIColor.systemGreen.cgColor
-                } else {
-                    
-                }
-                sender.view!.layer.shadowOpacity = 1
-                break
-            }
-            
-        case .ended:
-            UIView.animate(withDuration: 0.5, delay: 0.0,usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseIn]){
-               
-                self.dragEnded(draggableObject: sender, draggableZone: self.bowlZone)
-            }
-            
-        default:
-            break
-        }
-    }
-    
-    
+   
     
     func dragEnded(draggableObject: UIPanGestureRecognizer, draggableZone: UIView ) {
         
         if (draggableObject.view!.frame.intersects(draggableZone.frame)){
             draggableObject.view!.center = draggableZone.center
             draggableObject.view!.alpha = 0
+            
 //NOTE:            Play sounds
             
             
@@ -293,7 +204,17 @@ class SarapanViewController: UIViewController {
         draggableObject.view!.layer.shadowOpacity = 0
     }
     
-
- 
+    func sayCongratulationWhenFinish(){
+        if self.score == 3 {
+            print("Yes You Did It")
+        }
+    }
 
 }
+
+
+// MARK: What my code lacks of?
+// 1. Force unwrap (!)
+// 2. Inconsistency for Variable name (English or Indonesia, Choose one not both)
+// 3. Repeatable func to create View, i can make it as func with param to make it reusable
+
